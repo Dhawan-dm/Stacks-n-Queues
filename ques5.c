9c2c5db3a958827e2321ebe6b3d9ac882ee14126
Dhawan-dm <80594876+Dhawan-dm@users.noreply.github.com>
2021-09-10 16:32:11 +0530

    Update ques5.c

--- ques5.c
+++ ques5.c
@@ -3,7 +3,7 @@
 
 typedef struct {
     int top;
-    int mini;
+    int mini;  //top of min array
     int a[10000];
     int min[10000];
     
@@ -13,21 +13,21 @@ typedef struct {
 
 MinStack* minStackCreate() {
     
-    MinStack* stack = (MinStack*)malloc(sizeof(MinStack)*1);
+    MinStack* s = (MinStack*)malloc(sizeof(MinStack)*1);
     
-    stack->min[0] = INT_MAX;
-    stack->top = -1;
-    stack->mini = -1;
+    s->min[0] = INT_MAX;
+    s->top = -1;
+    s->mini = -1;
     
-    return stack;
+    return s;
     
 }
 
 void minStackPush(MinStack* obj, int x) {
     
-    int min = obj->min[obj->mini];
+    int small = obj->min[obj->mini];
     
-    if((x <= min) || (obj->mini == -1))
+    if((x <= small) || (obj->mini == -1))
     {
         obj->mini = obj->mini + 1;
         obj->min[obj->mini] = x;
@@ -69,5 +69,4 @@ void minStackFree(MinStack* obj) {
     free(obj);
     
 }
-
 
